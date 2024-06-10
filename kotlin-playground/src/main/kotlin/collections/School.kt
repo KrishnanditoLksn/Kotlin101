ff20a7f277bfb226e540745db6864e1791e3f73c
Gorthdzx <krislaksana123@gmail.com>
2024-06-10 10:05:17 +0700

    feat: sealed class

---
+++ kotlin-playground/src/main/kotlin/collections/School.kt
@@ -0,0 +1,39 @@
+package org.example.collections
+
+
+
+sealed class School {
+    class Classroom(classname: String, classCode: Int) : School(){
+        override fun toString(): String {
+            return super.toString()
+        }
+    }
+
+
+    data class Chair(val name: String, val width: Int, val height: Int) : School() {
+        override fun toString(): String {
+            return "Name $name\n" +
+                    "Width : $width\n" +
+                    "Height : $height\n"
+        }
+    }
+
+    data class Mading(val madingName: String, val madingCode: String) : School() {
+        override fun toString(): String {
+            return super.toString()
+        }
+    }
+
+    data class Book(val title: String, val authors: List<String>, val isbn: List<Int>) : School(){
+        override fun toString(): String {
+            return super.toString()
+        }
+    }
+}
+
+fun main() {
+    val book1 = School.Book("Kancil", mutableListOf("Dito"), mutableListOf(12312, 123))
+    val chair = School.Chair("Kursi Hijau", 200, 200)
+    println(chair.toString())
+    println(book1.toString())
+}
