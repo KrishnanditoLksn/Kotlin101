3785782e8fdc15161cae6558065d5a86457479e5
Gorthdzx <krislaksana123@gmail.com>
2024-09-07 22:12:53 +0700

    feat: lambda function

--- kotlin-playground/src/main/kotlin/functional_prog/HighOrderFunction.kt
+++ kotlin-playground/src/main/kotlin/functional_prog/HighOrderFunction.kt
@@ -8,25 +8,31 @@ fun main() {
 //        value + value
 //    }
     println(fizzBuzz(3))
+    println(sum(2))
+    println(printStr("Hehehe"))
 }
 
 fun printResult(value: Int, sum: (Int) -> Int): Int {
     return sum(value)
 }
 
-var sum: (Int) -> Int = { value -> value + value }
+var sum: (Int) -> Int = { value ->
+    value + value
+}
+
+val printStr: (String) -> String = { value ->
+    value
+}
 
 fun fizzBuzz(n: Int): List<String> {
     val answer = mutableListOf<String>()
     for (i in 1..n) {
         if (i % 3 == 0 && i % 5 == 0) {
             answer.add("FizzBuzz")
-        }
-        else if (i % 3 == 0) {
+        } else if (i % 3 == 0) {
             answer.add("Fizz")
 
-        }
-        else if (i % 5 == 0) {
+        } else if (i % 5 == 0) {
             answer.add("Buzz")
 
         } else {
