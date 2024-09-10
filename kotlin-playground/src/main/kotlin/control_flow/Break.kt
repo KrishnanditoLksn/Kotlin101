ac0b5893730ebd950393d9915f66a1035a002a81
Gorthdzx <krislaksana123@gmail.com>
2024-09-10 22:35:49 +0700

    feat: bulk code

--- kotlin-playground/src/main/kotlin/control_flow/Break.kt
+++ kotlin-playground/src/main/kotlin/control_flow/Break.kt
@@ -1,15 +1,26 @@
 package org.example.control_flow
 
+val a : List<Int> = mutableListOf(1,2,3)
 fun main() {
-    val listOfInt = listOf(1, 2, 3, null, 5, null, 7)
-    for (i in listOfInt) {
-        if (i == null)
-            continue
-        println(i)
-    }
-    breakLabels()
-}
+//    val listOfInt = listOf(1, 2, 3, null, 5, null, 7)
+//    for (i in listOfInt) {
+//        if (i == null)
+//            continue
+//        println(i)
+//    }
+//    breakLabels()
+//    println(code())
+//
+//    val ranges = 1.rangeTo(10)step 3
+//    ranges.forEach { _ ->
+//        println("Hello World")
+//    }
 
+    printResult(10){
+        value ->
+        value + value
+    }
+}
 /*
 loop labels
  */
@@ -22,4 +33,31 @@ fun breakLabels() {
         println("Inside Loop")
         if (j > 5)break@loop
     }
+
+    val message: String? = null
+    message?.also{
+        val length = it.length
+        val text = "text length $length"
+        println(text)
+    }
+}
+
+fun code():List<Int>{
+    return a.map {
+        it * 2
+    }
+}
+
+fun getmesg(name : String):String {
+    return "sd"
+}
+
+//inline fun printResult(value : Int , sum :  (Int) -> Int){
+//    val result = sum(value)
+//    println(result)
+//}
+
+inline fun printResult(value : Int , sum :(Int)->Int){
+    val res = sum(value)
+    println(res)
 }
--- kotlin-playground/src/main/kotlin/functional_prog/FunctionType.kt
+++ kotlin-playground/src/main/kotlin/functional_prog/FunctionType.kt
@@ -2,12 +2,12 @@ package org.example.functional_prog
 
 fun main() {
     val sum: Arithmetic = { valueA, valueB -> valueA + valueB }
-    val sumNull:Multiplication = {valueA ,valueB -> valueA * valueB}
+    val sumNull: Multiplication = { valueA, valueB -> valueA * valueB }
     val multiply: Arithmetic = { valueA, valueB -> valueA * valueB }
     val sumResult = sum.invoke(10, 10)
     println(sumResult)
 
-    val nullResult= sumNull?.invoke(10,10)
+    val nullResult = sumNull?.invoke(10, 10)
     println(nullResult)
 }
 
@@ -17,4 +17,4 @@ convert a function into data type using typealias keyword
 
 typealias Arithmetic = (Int, Int) -> Int
 
-typealias Multiplication=((Int,Int)->Int)?
+typealias Multiplication = ((Int, Int) -> Int)?
